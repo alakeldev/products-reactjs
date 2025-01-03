83e690f8f184a8fa2157aaea9bbd5832e371c787
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2025-01-03 19:52:01 +0100

    added some files to project + enhace product card style + added functionality to control over the showed desctiotion text

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ProductCard from "./components/ProductCard";
+import { productList } from "./data";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const renderProductList = productList.map((product) => <ProductCard key={product.id} product={product} />)
+
+  // another syntax for the above line but below when you want to call it inside div you call it renderProductList()
+  // const renderProductList = () => {
+  //       return productList.map((p) => <ProductCard key={p.id} />)
+  // }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="container">
+    <div className=" m-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md">
+      {renderProductList}
+    </div>
+    </main>
   )
 }
 
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,36 @@
+import { IProduct } from "../interfaces";
+import { textSlicer } from "../utils/functions";
+import Image from "./Image";
+import Button from "./ui/Button";
+
+interface IProps {
+  product: IProduct
+}
+
+const ProductCard = ({product}: IProps) => {
+  const {title, description, imageURL, price, category} = product;
+  return (
+    <div className="max-w-sm md:max-w-lg mx-auto md:mx-0 border rounded-md p-2 flex flex-col space-y-3">
+        <Image imageUrl= {imageURL} alt={"Product name"} className="rounded-md h-52 w-full lg:object-cover"/>
+        <h3 className="text-lg font-semibold">{title}</h3>
+        <p className="text-sm text-gray-500 break-words">{textSlicer(description)}</p>
+        <div className="flex items-center flex-wrap space-x-1">
+            <span className="w-5 h-5 bg-indigo-600 rounded-full cursor-pointer" ></span>
+            <span className="w-5 h-5 bg-yellow-600 rounded-full cursor-pointer" ></span>
+            <span className="w-5 h-5 bg-red-600 rounded-full cursor-pointer" ></span>
+        </div>
+        <div className="flex items-center justify-between">
+          <span>${price}</span>
+          <Image imageUrl= {category.imageURL} alt={category.name} className="w-10 h-10 rounded-full object-bottom"/>
+        </div>
+
+        <div className="flex items-center justify-between space-x-2">
+            <Button className="bg-indigo-400" width="w-full">Edit </Button> 
+            <Button className="bg-red-600 ">Delete</Button>
+        </div>
+
+    </div>
+  )
+}
+
+export default ProductCard;
